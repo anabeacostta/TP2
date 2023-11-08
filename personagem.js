1ede37dcdb5372cd4271ca248b1f9455f9622d43
anabeacostta <133997899+anabeacostta@users.noreply.github.com>
2023-11-08 17:40:43 -0300

    Add files via upload
    Eu criei a pagina onde você pode colocar seu nome e ficar salvo

---
+++ personagem.js
@@ -0,0 +1,11 @@
+document.getElementById("save-button").addEventListener("click", function () {
+    const characterName = document.getElementById("character-name").value;
+
+    if (characterName) {
+        const characterNameElement = document.createElement("h2");
+        characterNameElement.textContent = "Nome do Personagem: " + characterName;
+        document.getElementById("character-container").appendChild(characterNameElement);
+    } else {
+        alert("Por favor, insira um nome para o seu personagem.");
+    }
+});
